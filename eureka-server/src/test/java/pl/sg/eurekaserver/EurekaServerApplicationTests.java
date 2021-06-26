5603ced326b4810147d9ea58373585b40f61a417
szymek <szymon.gretka@gmail.com>
2021-06-26 21:00:33 +0200

    init eureka server

---
+++ eureka-server/src/test/java/pl/sg/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sg.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
